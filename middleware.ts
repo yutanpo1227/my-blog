1ff8be6d78cf8ed75744234749649e74589784e3
yutanpo1227 <komazawayuu1227@icloud.com>
2025-02-25 01:10:01 +0900

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!_next/static|images/:path*|thumbnails/:path*|favicon.ico|sitemap.xml|robots.txt).*)',
+    "/((?!_next/static|images/:path*|thumbnails/*.png|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
-}
+};
